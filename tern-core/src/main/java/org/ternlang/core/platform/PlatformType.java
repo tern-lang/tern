e57b83782435c84958e3250f951dc3eb9526a111
Authrus <authrus.root@gmail.com>
2019-03-29 21:20:48 +0100

    Update stack depth

--- tern-core/src/main/java/org/ternlang/core/platform/PlatformType.java
+++ tern-core/src/main/java/org/ternlang/core/platform/PlatformType.java
@@ -1,14 +1,22 @@
 package org.ternlang.core.platform;
 
 public enum PlatformType {
-   STANDARD("java.awt.Graphics"),
-   ANDROID("android.os.Build");
+   ANDROID("android.os.Build"),   
+   STANDARD("java.lang.ClassLoader");
    
    private final String type;
    
    private PlatformType(String type) {
       this.type = type;
    }
+   
+   public boolean isAndroid() {
+      return this == ANDROID;
+   }
+   
+   public boolean isStandard() {
+      return this == STANDARD;
+   }  
    
    public static PlatformType resolveType() {
       PlatformType[] types = PlatformType.values();
--- tern-core/src/main/java/org/ternlang/core/stack/ThreadLocalStack.java
+++ tern-core/src/main/java/org/ternlang/core/stack/ThreadLocalStack.java
@@ -1,9 +1,23 @@
 package org.ternlang.core.stack;
 
+import org.ternlang.core.platform.PlatformType;
+
 public class ThreadLocalStack extends ThreadLocal<StackTrace> {
+   
+   private static final int LARGE_STACK = 200;
+   private static final int SMALL_STACK = 40;
+   
+   private final PlatformType platform;
+   
+   public ThreadLocalStack() {
+      this.platform = PlatformType.resolveType();
+   }
 
    @Override
-   public StackTrace initialValue() {
-      return new StackTrace();
+   public StackTrace initialValue() {      
+      if(platform.isStandard()) {
+         return new StackTrace(LARGE_STACK);
+      }
+      return new StackTrace(SMALL_STACK);
    }
 }

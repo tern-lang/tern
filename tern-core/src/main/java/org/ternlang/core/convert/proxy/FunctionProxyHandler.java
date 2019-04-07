3e352cee7a35434ddbc347d52d1c894e0b63f56f
Authrus <authrus.root@gmail.com>
2019-04-07 22:56:04 +0100

    Update proxy invocation resolver

--- tern-core/src/main/java/org/ternlang/core/convert/proxy/FunctionProxyHandler.java
+++ tern-core/src/main/java/org/ternlang/core/convert/proxy/FunctionProxyHandler.java
@@ -1,9 +1,5 @@
 package org.ternlang.core.convert.proxy;
 
-import static org.ternlang.core.Reserved.METHOD_EQUALS;
-import static org.ternlang.core.Reserved.METHOD_HASH_CODE;
-import static org.ternlang.core.Reserved.METHOD_TO_STRING;
-
 import java.lang.reflect.Method;
 
 import org.ternlang.core.error.InternalStateException;
@@ -27,39 +23,15 @@ public class FunctionProxyHandler implements ProxyHandler {
    
    @Override
    public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
-      Object[] convert = extractor.extract(arguments);
       String name = method.getName();
+      Object[] convert = extractor.extract(arguments);
+      Invocation call = resolver.resolve(proxy, name, convert);
       int width = convert.length;
-      
-      if(name.equals(METHOD_HASH_CODE)) {
-         if(width != 0) {
-            throw new InternalStateException("Closure '"+ METHOD_HASH_CODE +"' does not accept " + width + " arguments");
-         }
-         return function.hashCode();
-      }
-      if(name.equals(METHOD_TO_STRING)) {
-         if(width != 0) {
-            throw new InternalStateException("Closure '"+METHOD_TO_STRING+"' does not accept " + width + " arguments");
-         }
-         return function.toString();
-      }
-      if(name.equals(METHOD_EQUALS)) {
-         if(width != 1) {
-            throw new InternalStateException("Closure '"+METHOD_EQUALS+"' does not accept " + width + " arguments");
-         }
-         return function.equals(convert[0]);
-      }
-      return invoke(proxy, name, convert);
-   }
-   
-   private Object invoke(Object proxy, String name, Object[] arguments) throws Throwable {
-      Invocation call = resolver.resolve(proxy, name, arguments);
-      int width = arguments.length;
       
       if(call == null) {
          throw new InternalStateException("Closure not matched with " + width +" arguments");
       }
-      Object data = call.invoke(null, proxy, arguments);
+      Object data = call.invoke(null, proxy, convert);
       
       if(data != null) {
          return wrapper.toProxy(data);
--- tern-core/src/main/java/org/ternlang/core/convert/proxy/FunctionProxyResolver.java
+++ tern-core/src/main/java/org/ternlang/core/convert/proxy/FunctionProxyResolver.java
@@ -1,5 +1,10 @@
 package org.ternlang.core.convert.proxy;
 
+import static org.ternlang.core.Reserved.METHOD_EQUALS;
+import static org.ternlang.core.Reserved.METHOD_HASH_CODE;
+import static org.ternlang.core.Reserved.METHOD_TO_STRING;
+
+import org.ternlang.core.error.InternalStateException;
 import org.ternlang.core.function.Function;
 import org.ternlang.core.function.Invocation;
 import org.ternlang.core.function.resolve.FunctionCall;
@@ -23,7 +28,16 @@ public class FunctionProxyResolver {
    
    public Invocation resolve(Object proxy, String name, Object[] arguments) throws Throwable {
       Type source = function.getSource();
-
+      
+      if(name.equals(METHOD_HASH_CODE)) {
+         return new HashCodeInvocation(function);
+      }
+      if(name.equals(METHOD_TO_STRING)) {
+         return new ToStringInvocation(function);
+      }
+      if(name.equals(METHOD_EQUALS)) {
+         return new EqualsInvocation(function);
+      }
       if(source != null) {
          Scope scope = source.getScope();
          FunctionCall call = resolver.resolveInstance(scope, proxy, name, arguments); 
@@ -35,6 +49,63 @@ public class FunctionProxyResolver {
       return resolver.resolveValue(value, arguments); // here arguments can be null!!!
    }
 
+   private static class HashCodeInvocation implements Invocation {
+
+      private final Function function;
+      
+      public HashCodeInvocation(Function function) {
+         this.function = function;
+      }
+      
+      @Override
+      public Object invoke(Scope scope, Object object, Object... arguments) throws Exception {
+         int width = arguments.length;
+         
+         if(width != 0) {
+            throw new InternalStateException("Closure '" + METHOD_HASH_CODE + "' does not accept " + width + " arguments");
+         }
+         return function.hashCode();
+      }      
+   }
+   
+   private static class ToStringInvocation implements Invocation {
+
+      private final Function function;
+      
+      public ToStringInvocation(Function function) {
+         this.function = function;
+      }
+      
+      @Override
+      public Object invoke(Scope scope, Object object, Object... arguments) throws Exception {
+         int width = arguments.length;
+         
+         if(width != 0) {
+            throw new InternalStateException("Closure '" + METHOD_TO_STRING + "' does not accept " + width + " arguments");
+         }
+         return function.toString();
+      }      
+   }
+   
+   private static class EqualsInvocation implements Invocation {
+
+      private final Function function;
+      
+      public EqualsInvocation(Function function) {
+         this.function = function;
+      }
+      
+      @Override
+      public Object invoke(Scope scope, Object object, Object... arguments) throws Exception {
+         int width = arguments.length;
+         
+         if(width != 1) {
+            throw new InternalStateException("Closure '" + METHOD_EQUALS + "' does not accept " + width + " arguments");
+         }
+         return function.equals(arguments[0]);
+      }      
+   }
+   
    private static class DefaultInvocation implements Invocation {
 
       private final FunctionCall call;

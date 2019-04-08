a422e6a635997744b12f69faf72b15e75850e5fa
Authrus <authrus.root@gmail.com>
2019-04-08 18:11:58 +0100

    Update converter

--- tern-core/src/main/java/org/ternlang/core/function/ArgumentConverterBuilder.java
+++ tern-core/src/main/java/org/ternlang/core/function/ArgumentConverterBuilder.java
@@ -17,8 +17,10 @@ import org.ternlang.core.type.Type;
 public class ArgumentConverterBuilder {
    
    private final ConstraintConverter converter;
+   private final ArgumentConverter none;
 
    public ArgumentConverterBuilder() {
+      this.none = new IdentityArgumentConverter();
       this.converter = new NullConverter();
    }
 
@@ -26,43 +28,60 @@ public class ArgumentConverterBuilder {
       int size = parameters.size();
 
       if(size > 0) {
-         Parameter parameter = parameters.get(size - 1);
-         boolean variable = parameter.isVariable();
-
+         Parameter last = parameters.get(size - 1);
+         boolean variable = last.isVariable();
+         
          return create(scope, parameters, variable);
       }
-      return create(scope, parameters, false);
+      return none;
    }
-
+   
    public ArgumentConverter create(Scope scope, List<Parameter> parameters, boolean variable) throws Exception {
+      int size = parameters.size();
+
+      if(size > 0) {
+         for(int i = 0; i < size; i++) {
+            Parameter parameter = parameters.get(i);
+            Constraint constraint = parameter.getConstraint();
+            Type type = constraint.getType(scope);
+         
+            if(type != null) {
+               return create(scope, parameters, variable, size);
+            }
+         }
+         if(variable) {
+            return create(scope, parameters, variable, size);
+         }
+      }
+      return new IdentityArgumentConverter(size);
+   }
+   
+   private ArgumentConverter create(Scope scope, List<Parameter> parameters, boolean variable, int size) throws Exception {
       Module module = scope.getModule();
       Context context = module.getContext();
       ConstraintMatcher matcher = context.getMatcher();
-      int size = parameters.size();
-      int none = 0;
       
       if(size > 0) {
          ConstraintConverter[] converters = new ConstraintConverter[size];
-
+   
          for(int i = 0; i < size - 1; i++) {
             Parameter parameter = parameters.get(i);
             Constraint constraint = parameter.getConstraint();
             Type type = constraint.getType(scope);
-
+   
             if(type != null) {
                converters[i] = matcher.match(type);
             } else {
                converters[i] = converter;
-               none++;
             }
          }
          Parameter parameter = parameters.get(size - 1);
          Constraint constraint = parameter.getConstraint();
          Type type = constraint.getType(scope);
-
+   
          if(type != null) {
             Type entry = type.getEntry();
-
+   
             if(variable) {
                converters[size - 1] = matcher.match(entry);
             } else {
@@ -70,15 +89,13 @@ public class ArgumentConverterBuilder {
             }
          } else {
             converters[size - 1] = converter;
-            none++;
          }
          if(variable) {
             return new VariableArgumentConverter(converters);
          }
-         if(none < size) {
-            return new FixedArgumentConverter(converters);
-         }
+         return new FixedArgumentConverter(converters);
       }
-      return new IdentityArgumentConverter(size);
+      return none;
    }
 }
+
